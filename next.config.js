ffa1c85fb3273ad229702d96912051641eddd043
real-projects11 <pabloalejandrodiaz2000@gmail.com>
2026-08-26 00:02:32 -0300

    Add files via upload

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async rewrites() {
+    return [
+      { source: '/', destination: '/index.html' },
+      { source: '/login', destination: '/login.html' },
+      { source: '/register', destination: '/register.html' },
+      { source: '/dashboard', destination: '/dashboard.html' },
+    ];
+  },
 };
 module.exports = nextConfig;
